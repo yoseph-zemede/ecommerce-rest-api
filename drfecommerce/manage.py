2f1f005e390d72237119c009dd761a35782cebfd
Yoseph Zemede <zemedeyoseph@gmail.com>
2024-08-13 00:02:00 +0300

    Setup formatting extensions

--- drfecommerce/manage.py
+++ drfecommerce/manage.py
@@ -4,13 +4,16 @@ import os
 import sys
 from drfecommerce.settings import base
 
+
 def main():
     """Run administrative tasks."""
 
     if base.DEBUG:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drfecommerce.settings.local')
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drfecommerce.settings.local")
     else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drfecommerce.settings.production')
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE", "drfecommerce.settings.production"
+        )
 
     try:
         from django.core.management import execute_from_command_line
@@ -23,5 +26,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
